129008b61cbc43c81f6e362b8831b9e2c57cefaa
Krisl1107 <Klagoda_zeya@mail.ru>
2025-03-03 09:25:43 +0700

    Update 13.py

--- 13.py
+++ 13.py
@@ -2,6 +2,7 @@ N=int(input())
 C=int(input())
 number=int(input())
 page=(number-1)//(N*C)+1
-column=(number-1)%(N*C)//N+1
-line=(number-1)%(N*C)//C+1
-print("Страница",page,"Стобец",column,"Сртока",line)
+column=(number-(page-1)*N*C)//N+1
+line=(number-(page-1)*N*C)-N*(column-1)
+print("Страница",page,"Стобец",column,"Строка",line)
+
